a9d4eef0132c356c111b0f7d82e52fed9ebdcfdf
Andreas <a.haselsteiner@uni-bremen.de>
2018-11-02 18:57:20 +0100

    First commit.

---
+++ organizers-code/dummy_plot_contour.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+def plot_contour(points):
+    """
+    Plots the environmental contour.
+    ----------
+    points : Class of points,
+        Description what the points are.
+    """
+
+	
+def compute_metric(points)
+    """
+    Computes something.
+    ----------
+    points : Class of points,
+        Description what the points are.
+    """
---
+++ participants-code/participant_1/dummy_fit_model.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.stats as sts
+
+
+def fit_model(dataset):
+    """
+    Fits the model to the dataset.
+    ----------
+    dataset : Class of dataset,
+        Description what the dataset is.
+    """
