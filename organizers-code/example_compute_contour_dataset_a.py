1e0911fb7c5572bf0c9cdb92f1ea7d63fdf18512
Andreas <a.haselsteiner@uni-bremen.de>
2018-12-11 15:06:59 +0100

    Add remaining datasets, add list of requirements for this code and improve the plotting function.

--- organizers-code/example_compute_contour_dataset_a.py
+++ organizers-code/example_compute_contour_dataset_a.py
@@ -14,10 +14,12 @@ with open('../datasets/A.txt', newline='') as csv_file:
             sample_tz.append(float(row[2]))
         idx = idx + 1
 
+# Define the structure of the probabilistic model that will be fitted to the
+# dataset. We will use the model that is recommended in DNV-RP-C205 (2010) on
+# page 38 and that is called 'conditonal modeling approach' (CMA).
 dist_description_hs = {'name': 'Weibull',
                       'dependency': (None, None, None),
                       'width_of_intervals': 0.5}
-
 dist_description_tz = {'name': 'Lognormal',
                       'dependency': (0,  None, 0), #Shape, Location, Scale
                       'functions': ('power3', None, 'exp3') #Shape, Location, Scale
@@ -25,12 +27,10 @@ dist_description_tz = {'name': 'Lognormal',
 
 my_fit = Fit((sample_hs, sample_tz),
              (dist_description_hs, dist_description_tz))
-
 iform_contour = IFormContour(my_fit.mul_var_dist, 50, 1, 100)
-
 plot_contour(iform_contour.coordinates[0][1],
              iform_contour.coordinates[0][0],
              50,
-             'zero-crossing period (s)',
+             'zero-up-crossing period (s)',
              'significant wave height (m)',
              [sample_tz, sample_hs])
--- organizers-code/plot.py
+++ organizers-code/plot.py
@@ -1,5 +1,3 @@
-from plot_generic import alpha_shape, convert_ndarray_list_to_multipoint
-from descartes import PolygonPatch
 import matplotlib.pyplot as plt
 
 def plot_sample(x, y, ax):
@@ -22,6 +20,8 @@ def plot_contour(x, y, return_period, x_label='X1', y_label='X2', sample=None):
     """
     Plots the environmental contour.
 
+    The method expects the coordinates to be ordered by angle.
+
     Parameters
     ----------
     x : ndarray of doubles,
@@ -37,33 +37,30 @@ def plot_contour(x, y, return_period, x_label='X1', y_label='X2', sample=None):
     sample : list of lists of floats,
         Sample of environmental states.
     """
+    xplot = x.tolist()
+    xplot.append(x[0])
+    yplot = y.tolist()
+    yplot.append(y[0])
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(5,5), dpi=150)
     ax = fig.add_subplot(111)
     if sample:
         plot_sample(sample[0], sample[1], ax)
-    ax.scatter(x, y, s=15, c='b',
-               label='extreme condition')
-    try:
-        alpha = .3
-        concave_hull, edge_points = alpha_shape(
-            convert_ndarray_list_to_multipoint([x, y]),
-            alpha=alpha)
-        patch_design_region = PolygonPatch(
-            concave_hull, fc='#999999', linestyle='None', fill=True,
-            zorder=-2, label='design region')
-        ec_label = str(return_period) + ' year env. contour'
-        patch_environmental_contour = PolygonPatch(
-            concave_hull, ec='b', fill=False, zorder=-1,
-            label=ec_label)
-        ax.add_patch(patch_design_region)
-        ax.add_patch(patch_environmental_contour)
-    except(ZeroDivisionError):  # alpha_shape() can throw these
-        print('Encountered a ZeroDivisionError when using alpha_shape.'
-              'Consequently no contour is plotted.')
-
-    plt.legend(loc='upper right')
+    ec_label = str(return_period) + ' year contour'
+    ax.plot(xplot, yplot, c='b', label=ec_label)
+    plt.legend(loc='upper left', frameon=False)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    if sample:
+        upper_ylim = max(max(y), max(sample[1])) * 1.2
+    else:
+        upper_ylim = max(y) * 1.2
+    plt.ylim((0, upper_ylim))
+
+    # Remove axis on the right and on the top (Matlab 'box off').
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
 
     plt.show()
