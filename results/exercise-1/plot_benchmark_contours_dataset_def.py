6a7b6aa18c333e689ffc4370008f8bcbf3395446
Ryan Coe <rcoe@sandia.gov>
2020-09-02 09:21:16 -0600

    use subplots and some stylistic updates
    use colorbrewer for contour line colors
    export as vector PDF with raster points

--- results/exercise-1/plot_benchmark_contours_dataset_def.py
+++ results/exercise-1/plot_benchmark_contours_dataset_def.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from palettable.colorbrewer.qualitative import Paired_9 as mycorder
 from viroconcom.read_write import read_ecbenchmark_dataset, read_contour
 from viroconcom.plot import plot_contour
 
@@ -10,7 +10,10 @@ lastname_firstname = ['Wei_Bernt', 'GC_CGS', 'hannesdottir_asta',
                       'haselsteiner_andreas', 'BV', 'mackay_ed',
                       'qiao_chi', 'rode_anna', 'vanem_DirectSampling',
                       'vanem_DirectSamplingsmoothed', 'vanem_IFORM']
-styles_for_contribution = ['-b', '-r', '-g', '-k', '--b', '--r', '--g', '--k', '-c', '--c', '.-c']
+styles_for_contribution = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '--', '.-']
+colors_for_contribution = mycorder.mpl_colors
+for idx in range(3):
+        colors_for_contribution.append(colors_for_contribution[8])
 legends_for_contribution = ['Contribution 1',
                           'Contribution 2',
                           'Contribution 3',
@@ -25,8 +28,9 @@ legends_for_contribution = ['Contribution 1',
                           ]
 n_contours_to_analyze = 11
 
-for dataset_char in dataset_chars:
-    for return_period in return_periods:
+fig, ax = plt.subplots(len(return_periods), len(dataset_chars), sharex='row', sharey='row', figsize=(10, 8))
+for (return_period, ax0) in zip(return_periods, ax):
+    for (dataset_char, ax1) in zip(dataset_chars, ax0):
         # Load the environmental data.
         file_name = 'datasets/' + dataset_char + '.txt'
         sample_v, sample_hs, label_v, label_hs = read_ecbenchmark_dataset(file_name)
@@ -38,7 +42,7 @@ for dataset_char in dataset_chars:
             contribution_nr = i + 1
             if contribution_nr > 9:
                 contribution_nr = 9
-            folder_name = 'results/exercise-1/contribution-' + str(contribution_nr)
+            folder_name = 'results/exercise-1/participant-' + str(contribution_nr)
             file_name = folder_name + '/' + lastname_firstname[i] + '_dataset_' + \
                         dataset_char + '_' + str(return_period) + '.txt'
             if contribution_nr in (1, 2, 3, 5, 6, 8):
@@ -50,17 +54,37 @@ for dataset_char in dataset_chars:
             max_hs_on_contours[i] = max(hs[~np.isnan(hs)])
 
         # Plot the data and the contour.
-        fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-        plt.scatter(sample_v, sample_hs, c='black', alpha=0.5)
+        # fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+        ax1.scatter(sample_v, sample_hs, c='black', alpha=0.5, zorder=-2)
         for i in range(n_contours_to_analyze):
             ylim = 1.05 * max([max(max_hs_on_contours), max(sample_hs)])
+            # plot_contour(contours_v[i], contours_hs[i],
+            #              ax=ax, x_label=label_v.capitalize(), y_label=label_hs.capitalize(),
+            #              line_style=styles_for_contribution[i],
+            #              contour_label=legends_for_contribution[i],
+            #              upper_ylim=ylim)
             plot_contour(contours_v[i], contours_hs[i],
-                         ax=ax, x_label=label_v.capitalize(), y_label=label_hs.capitalize(),
+                         ax=ax1,
                          line_style=styles_for_contribution[i],
-                         contour_label=legends_for_contribution[i],
+                         color=colors_for_contribution[i],
                          upper_ylim=ylim)
-            plt.legend(prop={'size': 6})
-            plt.title('Dataset ' + dataset_char + ', ' + str(return_period) + '-year contour')
-        plt.show()
-        fig.savefig('results/e1_overlay_dataset_' + dataset_char +
-                    '_returnperiod_' + str(return_period), dpi=150)
+        #     plt.legend(prop={'size': 6})
+        #     plt.title('Dataset ' + dataset_char + ', ' + str(return_period) + '-year contour')
+        # plt.show()
+        # fig.savefig('e1_overlay_dataset_' + dataset_char + '_returnperiod_' +
+        #             str(return_period), dpi=150)
+
+        ax1.set_rasterization_zorder(-1)
+        ax1.set_xlabel(label_v.capitalize())
+        ax1.set_ylabel(label_hs.capitalize())
+        ax1.set_title('Dataset ' + dataset_char + ', ' + str(return_period) + '-year contour')
+        
+lgd = fig.legend(legends_for_contribution, 
+           loc='lower center', 
+           ncol=6, 
+           prop={'size': 8})
+fig.tight_layout(rect=(0,0.05,1,1))
+plt.savefig('results/e1_overlay_def.pdf', bbox_inches='tight', bbox_extra_artists=[lgd])
+plt.show()
+
+
