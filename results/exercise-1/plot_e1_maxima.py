51d73bb9a89e6ac8622181219cea514c30860761
Ryan Coe <rcoe@sandia.gov>
2020-09-23 09:48:41 -0600

    split abc and def into two plots

--- results/exercise-1/plot_e1_maxima.py
+++ results/exercise-1/plot_e1_maxima.py
@@ -86,61 +86,75 @@ for i in range(11):
                 f_max_hs.append(max(hs))
 
 
-fig, axs = plt.subplots(2, 2, figsize=(12*0.75, 9*0.75))
+fig1, axs1 = plt.subplots(1, 2, figsize=(8*0.75, 4.5*0.75))
 values = range(len(a_max_hs)) + np.ones(len(a_max_hs))
 marker_size = 60
 
 
-scatterHs = axs[0, 0].scatter(np.ones(np.shape(a_max_hs)), a_max_hs, c=values, s=marker_size,
-                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 0].scatter(np.ones(np.shape(b_max_hs)) + 1, b_max_hs, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 0].scatter(np.ones(np.shape(c_max_hs)) + 2, c_max_hs, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 0].set_xticks([1, 2, 3])
-axs[0, 0].set_xticklabels(['A', 'B', 'C'])
-axs[0, 0].set_ylabel('Max. Hs along 20-yr contour (m)')
+scatterHs = axs1[0].scatter(np.ones(np.shape(a_max_hs)), a_max_hs, c=values, s=marker_size,
+                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[0].scatter(np.ones(np.shape(b_max_hs)) + 1, b_max_hs, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[0].scatter(np.ones(np.shape(c_max_hs)) + 2, c_max_hs, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[0].set_xticks([1, 2, 3])
+axs1[0].set_xticklabels(['A', 'B', 'C'])
+axs1[0].set_ylabel('Max. Hs along 20-yr contour (m)')
 
 
-scatterTp = axs[0, 1].scatter(np.ones(np.shape(a_min_tz)), a_min_tz, c=values, s=marker_size,
-                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].scatter(np.ones(np.shape(b_min_tz)) + 1, b_min_tz, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].scatter(np.ones(np.shape(c_min_tz)) + 2, c_min_tz, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].scatter(np.ones(np.shape(a_min_tz)) + 0.2, a_max_tz, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].scatter(np.ones(np.shape(b_min_tz)) + 1.2, b_max_tz, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].scatter(np.ones(np.shape(c_min_tz)) + 2.2, c_max_tz, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[0, 1].set_xticks([1.1, 2.1, 3.1])
-axs[0, 1].set_xticklabels(['A', 'B', 'C'])
-axs[0, 1].set_ylabel('Min. and max Tz  along 20-yr contour (s)')
-
-scatterV = axs[1, 0].scatter(np.ones(np.shape(d_max_v)), d_max_v, c=values, s=marker_size,
-                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 0].scatter(np.ones(np.shape(e_max_v)) + 1, e_max_v, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 0].scatter(np.ones(np.shape(f_max_v)) + 2, f_max_v, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 0].set_xticks([1, 2, 3])
-axs[1, 0].set_xticklabels(['D', 'E', 'F'])
-axs[1, 0].set_ylabel('Max. V  along 50-yr contour (m/s)')
+scatterTp = axs1[1].scatter(np.ones(np.shape(a_min_tz)), a_min_tz, c=values, s=marker_size,
+                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].scatter(np.ones(np.shape(b_min_tz)) + 1, b_min_tz, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].scatter(np.ones(np.shape(c_min_tz)) + 2, c_min_tz, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].scatter(np.ones(np.shape(a_min_tz)) + 0.2, a_max_tz, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].scatter(np.ones(np.shape(b_min_tz)) + 1.2, b_max_tz, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].scatter(np.ones(np.shape(c_min_tz)) + 2.2, c_max_tz, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs1[1].set_xticks([1.1, 2.1, 3.1])
+axs1[1].set_xticklabels(['A', 'B', 'C'])
+axs1[1].set_ylabel('Min. and max Tz  along 20-yr contour (s)')
 
 
-scatterHs = axs[1, 1].scatter(np.ones(np.shape(d_max_hs)), d_max_hs, c=values, s=marker_size,
-                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 1].scatter(np.ones(np.shape(e_max_hs)) + 1, e_max_hs, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 1].scatter(np.ones(np.shape(f_max_hs)) + 2, f_max_hs, c=values, s=marker_size,
-                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7)
-axs[1, 1].set_xticks([1, 2, 3])
-axs[1, 1].set_xticklabels(['D', 'E', 'F'])
-axs[1, 1].set_ylabel('Max. Hs  along 50-yr contour  (m)')
+fig2, axs2 = plt.subplots(1, 2, figsize=(8*0.75, 4.5*0.75))
 
-fig.legend(handles=scatterHs.legend_elements()[0], labels=legends_for_contribution,
-           prop={'size': 6}, loc='lower center', ncol=6, scatterpoints=1)
+scatterV = axs2[0].scatter(np.ones(np.shape(d_max_v)), d_max_v, c=values, s=marker_size,
+                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[0].scatter(np.ones(np.shape(e_max_v)) + 1, e_max_v, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[0].scatter(np.ones(np.shape(f_max_v)) + 2, f_max_v, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[0].set_xticks([1, 2, 3])
+axs2[0].set_xticklabels(['D', 'E', 'F'])
+axs2[0].set_ylabel('Max. V  along 50-yr contour (m/s)')
 
+
+scatterHs = axs2[1].scatter(np.ones(np.shape(d_max_hs)), d_max_hs, c=values, s=marker_size,
+                              cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[1].scatter(np.ones(np.shape(e_max_hs)) + 1, e_max_hs, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[1].scatter(np.ones(np.shape(f_max_hs)) + 2, f_max_hs, c=values, s=marker_size,
+                  cmap=mycorder.mpl_colormap, edgecolors='k', alpha=0.7, linewidths=0.5)
+axs2[1].set_xticks([1, 2, 3])
+axs2[1].set_xticklabels(['D', 'E', 'F'])
+axs2[1].set_ylabel('Max. Hs  along 50-yr contour  (m)')
+
+fig1.legend(handles=scatterHs.legend_elements()[0], labels=legends_for_contribution,
+           prop={'size': 6}, loc='lower center', ncol=5, scatterpoints=1)
+fig2.legend(handles=scatterHs.legend_elements()[0], labels=legends_for_contribution,
+           prop={'size': 6}, loc='lower center', ncol=5, scatterpoints=1)
+
+# lgd = fig.legend(legends_for_contribution, 
+#            loc='lower center', 
+#            ncol=6, 
+#            prop={'size': 8})
+# fig.tight_layout(rect=(0,0.05,1,1))
+
+fig1.tight_layout(rect=(0,0.1,1,1))
+fig2.tight_layout(rect=(0,0.1,1,1))
 # plt.show()
-fig.savefig('results/e1_max_values.pdf')
+fig1.savefig('results/e1_max_values_abc.pdf', bbox_inches='tight')
+fig2.savefig('results/e1_max_values_def.pdf', bbox_inches='tight')
