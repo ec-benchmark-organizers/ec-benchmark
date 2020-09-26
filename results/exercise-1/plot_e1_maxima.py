36a279a9d07ab66d592e17b6f8ba40e3ba4d10ee
Andreas <a.haselsteiner@uni-bremen.de>
2020-09-26 22:53:59 +0200

    Also plot 1-yr empirical return values.

--- results/exercise-1/plot_e1_maxima.py
+++ results/exercise-1/plot_e1_maxima.py
@@ -9,26 +9,22 @@ lastname_firstname = ['Wei_Bernt', 'GC_CGS', 'hannesdottir_asta',
                       'haselsteiner_andreas', 'BV', 'mackay_ed',
                       'qiao_chi', 'rode_anna', 'vanem_DirectSampling',
                       'vanem_DirectSamplingsmoothed', 'vanem_IFORM']
-legends_for_contribution = np.array(['Contribution 1',
-                          'Contribution 2',
-                          'Contribution 3',
-                          'Contribution 4',
-                          'Contribution 5',
-                          'Contribution 6',
-                          'Contribution 7',
-                          'Contribution 8',
-                          'Contribution 9, DS',
-                          'Contribution 9, DS smoothed',
-                          'Contribution 9, IFORM'
-                          ])
+legends_for_contribution = [
+    'Contr. 1',
+    'Contr. 2',
+    'Contr. 3',
+    'Contr. 4',
+    'Contr. 5',
+    'Contr. 6',
+    'Contr. 7',
+    'Contr. 8',
+    'Contr. 9, DS',
+    'Contr. 9, DS s.',
+    'Contr. 9, IFORM']
 
 colors_for_contribution = np.array(mycorder.mpl_colors)
 for idx in range(3):
     np.append(colors_for_contribution, colors_for_contribution[8])
-color_dict = {}
-keys = range(11)
-for i in keys:
-    color_dict[i] = colors_for_contribution[i]
 # Marginal exceedance = 'o', total exceedance = 'v'
 marker_class0 = 'o'
 marker_class1 = 'v'
@@ -163,6 +159,8 @@ for i in range(11):
 empirical_max_hs_abc = np.empty([3, 1])
 empirical_min_tz_abc = np.empty([3, 1])
 empirical_max_tz_abc = np.empty([3, 1])
+empirical_hs1_abc = np.empty([3, 1])
+empirical_tz1_abc = np.empty([3, 1])
 for i, dataset_char in np.ndenumerate(['A', 'B', 'C']):
     file_name_provided = 'datasets/' + dataset_char + '.txt'
     file_name_retained = 'datasets-retained/' + dataset_char + 'r.txt'
@@ -170,18 +168,30 @@ for i, dataset_char in np.ndenumerate(['A', 'B', 'C']):
         file_name_provided)
     hs_r, tz_r, lhs, ltz = read_ecbenchmark_dataset(
         file_name_retained)
-    empirical_max_hs_abc[i] = max([max(hs_p), max(hs_r)])
-    empirical_min_tz_abc[i] = max([min(tz_p), min(tz_r)])
-    empirical_max_tz_abc[i] = max([max(tz_p), max(tz_r)])
+    hs = np.append(hs_p, hs_r)
+    tz = np.append(tz_p, tz_r)
+    empirical_max_hs_abc[i] = max(hs)
+    empirical_min_tz_abc[i] = min(tz)
+    empirical_max_tz_abc[i] = max(tz)
+    pe_1yr = 1.0 / (365.25 * 24)
+    empirical_hs1_abc[i] = np.quantile(hs, 1 - pe_1yr)
+    empirical_tz1_abc[i] = np.quantile(tz, 1 - pe_1yr)
 empirical_max_v_def = np.empty([3, 1])
 empirical_max_hs_def = np.empty([3, 1])
+empirical_v1_def = np.empty([3, 1])
+empirical_hs1_def = np.empty([3, 1])
 for i, dataset_char in np.ndenumerate(['D', 'E', 'F']):
     file_name_provided = 'datasets/' + dataset_char + '.txt'
     file_name_retained = 'datasets-retained/' + dataset_char + 'r.txt'
     v_p, hs_p, lv, lhs = read_ecbenchmark_dataset(file_name_provided)
     v_r, hs_r, lv, lhs = read_ecbenchmark_dataset(file_name_retained)
-    empirical_max_v_def[i] = max([max(v_p), max(v_r)])
-    empirical_max_hs_def[i] = max([max(hs_p), max(hs_r)])
+    v = np.append(v_p, v_r)
+    hs = np.append(hs_p, hs_r)
+    empirical_max_v_def[i] = max(v)
+    empirical_max_hs_def[i] = max(hs)
+    pe_1yr = 1.0 / (365.25 * 24)
+    empirical_v1_def[i] = np.quantile(v, 1 - pe_1yr)
+    empirical_hs1_def[i] = np.quantile(hs, 1 - pe_1yr)
 
 # Plot the figure for datasets A, B, C.
 fig_abc, axs = plt.subplots(2, 2, figsize=(10*0.75, 9*0.75))
@@ -199,8 +209,8 @@ for i in (0, 1):
     axs[0, 0].scatter(np.ones(np.shape(c_max_hs_c1[classes==i])) + 2, c_max_hs_c1[classes==i],
                       marker=marker_class[i], c=colors_for_contribution[classes==i], s=marker_size,
                       edgecolors='k', alpha=0.7, linewidths=0.5)
-#for i in (0, 1, 2):
-    #axs[0, 0].plot([i + 0.8, i + 1.2], [empirical_max_hs_abc[i], empirical_max_hs_abc[i]], '-k')
+for i in range(3):
+    axs[0, 0].plot([i + 0.8, i + 1.2], [empirical_hs1_abc[i], empirical_hs1_abc[i]], '--k', dashes=(2, 1))
 axs[0, 0].spines['right'].set_visible(False)
 axs[0, 0].spines['top'].set_visible(False)
 axs[0, 0].yaxis.set_ticks_position('left')
@@ -229,9 +239,8 @@ for i in (0, 1):
     axs[0, 1].scatter(np.ones(np.shape(c_min_tz_c1[classes==i])) + 2.2, c_max_tz_c1[classes==i],
                       marker=marker_class[i], c=colors_for_contribution[classes==i],
                       s=marker_size, edgecolors='k', alpha=0.7, linewidths=0.5)
-#for i in range(3):
-    #axs[0, 1].plot([i + 0.9, i + 1.1], [empirical_min_tz_abc[i], empirical_min_tz_abc[i]], '-k')
-    #axs[0, 1].plot([i + 1.1, i + 1.3], [empirical_max_tz_abc[i], empirical_max_tz_abc[i]], '-k')
+for i in range(3):
+    axs[0, 1].plot([i + 1.0, i + 1.4], [empirical_tz1_abc[i], empirical_tz1_abc[i]], '--k', dashes=(2, 1))
 
 # Remove axis on the right and on the top (Matlab 'box off').
 axs[0, 1].spines['right'].set_visible(False)
@@ -292,8 +301,8 @@ for i in (0, 1):
                       marker=marker_class[i], c=colors_for_contribution[classes==i],
                       s=marker_size, edgecolors='k', alpha=0.7, linewidths=0.5)
 for i in range(3):
-    axs[1, 1].plot([i + 0.9, i + 1.1], [empirical_min_tz_abc[i], empirical_min_tz_abc[i]], '-k')
-    axs[1, 1].plot([i + 1.1, i + 1.3], [empirical_max_tz_abc[i], empirical_max_tz_abc[i]], '-k')
+    axs[1, 1].plot([i + 0.8, i + 1.2], [empirical_min_tz_abc[i], empirical_min_tz_abc[i]], '-k')
+    axs[1, 1].plot([i + 1.0, i + 1.4], [empirical_max_tz_abc[i], empirical_max_tz_abc[i]], '-k')
 
 # Remove axis on the right and on the top (Matlab 'box off').
 axs[1, 1].spines['right'].set_visible(False)
@@ -313,10 +322,14 @@ for i in range(n_contributions):
         Line2D([0], [0], marker=marker_class[classes[i]], c='w', ms=7,
                markerfacecolor=colors_for_contribution[i],
                markeredgecolor='k', alpha=0.7, linewidth=0.3))
+handles.append(Line2D([0], [0], c='k', ls='--', dashes=(2,1)))
 handles.append(Line2D([0], [0], c='k'))
-labels = np.append(legends_for_contribution, 'Max/Min in full dataset').tolist()
+
+labels = legends_for_contribution.copy()
+labels.append('Empirical marginal 1-yr return value')
+labels.append('Max/Min in full dataset')
 fig_abc.legend(handles=handles, labels=labels,
-           prop={'size': 6}, loc='lower center', ncol=6, scatterpoints=1)
+           prop={'size': 6}, loc='lower center', ncol=7, scatterpoints=1)
 fig_abc.tight_layout(rect=(0, 0.05, 1, 1))
 
 # Plot the figure for datasets D, E F.
@@ -334,8 +347,8 @@ for i in (0, 1):
                       c=colors_for_contribution[classes==i], s=marker_size, marker=marker_class[i],
                       edgecolors='k', alpha=0.7, linewidths=0.5)
 
-#for i in (0, 1, 2):
-#    axs[0, 0].plot([i + 0.8, i + 1.2], [empirical_max_v_def[i], empirical_max_v_def[i]], '-k')
+for i in  range(3):
+    axs[0, 0].plot([i + 0.8, i + 1.2], [empirical_v1_def[i], empirical_v1_def[i]], '-k', dashes=(2,1))
 axs[0, 0].spines['right'].set_visible(False)
 axs[0, 0].spines['top'].set_visible(False)
 axs[0, 0].yaxis.set_ticks_position('left')
@@ -355,8 +368,8 @@ for i in (0, 1):
     axs[0, 1].scatter(np.ones(np.shape(f_max_hs_c1[classes==i])) + 2, f_max_hs_c1[classes==i],
                       c=colors_for_contribution[classes==i], s=marker_size, marker=marker_class[i],
                       edgecolors='k', alpha=0.7, linewidths=0.5)
-#for i in range(3):
-#    axs[0, 1].plot([i + 0.8, i + 1.2], [empirical_max_hs_def[i], empirical_max_hs_def[i]], '-k')
+for i in range(3):
+    axs[0, 1].plot([i + 0.8, i + 1.2], [empirical_hs1_def[i], empirical_hs1_def[i]], '-k', dashes=(2,1))
 axs[0, 1].spines['right'].set_visible(False)
 axs[0, 1].spines['top'].set_visible(False)
 axs[0, 1].yaxis.set_ticks_position('left')
@@ -410,9 +423,11 @@ axs[1, 1].set_xticklabels(['D', 'E', 'F'])
 axs[1, 1].set_ylim(6, 20)
 axs[1, 1].set_ylabel('Max. Hs  along 50-yr contour  (m)')
 
-labels = np.append(legends_for_contribution, 'Maximum in full dataset').tolist()
+labels = legends_for_contribution.copy()
+labels.append('Empirical marginal 1-yr return value')
+labels.append('Maximum in full dataset')
 fig_def.legend(handles=handles, labels=labels,
-           prop={'size': 6}, loc='lower center', ncol=6, scatterpoints=1)
+           prop={'size': 6}, loc='lower center', ncol=7, scatterpoints=1)
 fig_def.tight_layout(rect=(0, 0.05, 1, 1))
 
 plt.show()
