274d8957fbeeecef6b169cbbefa2a5b94455ca0d
Andreas <a.haselsteiner@uni-bremen.de>
2020-12-01 14:50:13 +0100

    Improve lambda script by using newest vriconcom

--- results/discussion/extremal_dependence.py
+++ results/discussion/extremal_dependence.py
@@ -1,10 +1,12 @@
-# Work in progress!
-
 from viroconcom.params import ConstantParam, FunctionParam
 from viroconcom.distributions import ExponentiatedWeibullDistribution, MultivariateDistribution
 
-u = 0.9999
-#u = 1 - 1 / (50 * 365.25 * 24)
+#u = 1 - 1E-2 # lambda = 0.526
+u = 1 - 1E-3 # lambda = 0.367, 0.442
+#u = 1 - 1E-4 # lambda =  0.283, 0.298
+#u = 1 - 1E-5 # 
+#u = 1 - 1 / (1 * 365.25 * 24) # lambda = 0.331
+#u = 1 - 1 / (50 * 365.25 * 24) # lambda = 0.173
 
 # Create distribution.
 U10 = ExponentiatedWeibullDistribution(shape=2.42, scale=10.0, shape2=0.761)
@@ -18,10 +20,8 @@ dependencies = [(None, None, None, None) , (0, None, 0, None) ]
 mul_dist = MultivariateDistribution(distributions, dependencies)
 
 ur = U10.i_cdf(u)
-#hr = mul_dist.marginal_icdf(u, dim=1)
-hr = 8.5
-print('u_r = ' + str(ur) + ', h_r = ' + str(hr))
+hr = mul_dist.marginal_icdf(u, dim=1)
+print(f'u_r = {ur:.3f}, h_r = {h3:.3f}')
 jointAB = 1 - U10.cdf(ur) - mul_dist.marginal_cdf(hr, dim=1) + mul_dist.cdf([ur, hr], lower_integration_limit=(0, 0))
-print('jointAB = ' + str(jointAB))
 lam = jointAB / (1 - u)
-print('lambda = ' + str(lam))
+print(f'lambda = {lam[0]:.3f}')
