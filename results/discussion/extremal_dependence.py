b0d2f2172c52a6d97edf02fc75bd6c7abd9d8c5b
Andreas <a.haselsteiner@uni-bremen.de>
2020-12-02 12:34:01 +0100

    Improve variable naming, write down results.

--- results/discussion/extremal_dependence.py
+++ results/discussion/extremal_dependence.py
@@ -6,18 +6,18 @@ from viroconcom.distributions import (WeibullDistribution,
 from viroconcom.read_write import read_ecbenchmark_dataset
 
 # Results: 
-#u = 1 - 1E-2 # lambda contr. 1 & 2: 0.481, 0.475, 0.461| contr. 4 = 0.526, 0.526, 0.524 | empirical (p, r): 0.552, 0.851
-#u = 1 - 1E-3 # lambda contr. 1 & 2: 0.405 | contr. 4 = 0.415, 0.405, 0.406 | empirical (p, r): 0.571, 0.891
-#u = 1 - 1E-4 # lambda contr. 1 & 2: 0.378 | contr. 4 = 0.292, 0.291, 0.302 | empirical (p, r): 0.814, 0.572
-#u = 1 - 1E-5 # lambda contr. 1 & 2: X.XXX | contr. 4 = 0.215, 0.211, 0.213 | empirical (p, r): -1.119
+#alpha = 1E-2 # lambda contr. 1&2: 0.481, 0.475, 0.461 | contr. 4 = 0.526, 0.526, 0.524 | empirical (p, r): 0.552, 0.851
+#alpha = 1E-3 # lambda contr. 1&2: 0.433, 0.409, 0.445 | contr. 4 = 0.415, 0.405, 0.406 | empirical (p, r): 0.571, 0.891
+#alpha = 1E-4 # lambda contr. 1&2: 0.371, 0.308, 0.321 | contr. 4 = 0.292, 0.291, 0.302 | empirical (p, r): 0.814, 0.572
+#alpha = 1E-5 # lambda contr. 1&2: X.XXX | contr. 4 = 0.215, 0.211, 0.213 | empirical (p, r): -1.119
 
-us = 1 - np.array([1E-2, 1E-3, 1E-4])
+alphas = np.array([1E-2, 1E-3, 1E-4])
 
 # Create joint distribution model of contribution #1 and #2 (baseline model)
-alpha = FunctionParam('power3', 0, 7.58, 0.520)
-beta = FunctionParam('power3', 0, 3.89, 0.497)
+u_scale = FunctionParam('power3', 0, 7.58, 0.520)
+u_shape = FunctionParam('power3', 0, 3.89, 0.497)
 Hs = WeibullDistribution(scale=1.58, shape=1.41, loc=1.02)
-U10 = WeibullDistribution(scale=alpha, shape=beta)
+U10 = WeibullDistribution(scale=u_scale, shape=u_shape)
 distributions = [Hs, U10]
 dependencies = [(None, None, None), (0, None, 0)]
 joint_model_1 = MultivariateDistribution(distributions, dependencies)
@@ -36,26 +36,26 @@ joint_model_4 = MultivariateDistribution(distributions, dependencies)
 joint_models = [joint_model_1, joint_model_4]
 model_names = ['Contribution 1 and 2', 'Contribution 4']
 
-for u in us:
-    print(f'*** Computing with u = 1 - {1-u:.1e} ***') 
+for alpha in alphas:
+    print(f'*** Computing with alpha = {alpha:.1e} ***') 
     for j, joint_model in enumerate(joint_models):
         print(f'* Joint model: {model_names[j]} *')    
         n_iters = 3
         for i in range(n_iters):
-            ur = joint_model.marginal_icdf(u, dim=0)
-            hr = joint_model.marginal_icdf(u, dim=1, precision_factor=5)
+            ur = joint_model.marginal_icdf(1 - alpha, dim=0)
+            hr = joint_model.marginal_icdf(1- alpha, dim=1, precision_factor=5)
             print(f'i = {i+1}/{n_iters}')
             print(f'u_r = {ur:.3f}, h_r = {hr:.3f}')
-            jointAB = 1 - 2 * u + joint_model.cdf([ur, hr], lower_integration_limit=(0, 0))
-            lam = jointAB / (1 - u)
+            jointAB = 1 - 2 * (1- alpha) + joint_model.cdf([ur, hr], lower_integration_limit=(0, 0))
+            lam = jointAB / alpha
             print(f'model lambda = {lam:.3f}')
 
     file_names = ['datasets/D.txt', 'datasets-retained/Dr.txt']
     file_labels = ['D_provided', 'D_retained']
     for i, fname in enumerate(file_names):
         u10, hs, lhs, ltz = read_ecbenchmark_dataset(fname)
-        ur = np.quantile(u10, u)
-        hr = np.quantile(hs, u)
-        jointAB = 1 - 2 * u + joint_model.cdf([ur, hr], lower_integration_limit=(0, 0))
-        lam = jointAB / (1 - u)
+        ur = np.quantile(u10, 1 - alpha)
+        hr = np.quantile(hs, 1 - alpha)
+        jointAB = 1 - 2 * (1 - alpha) + joint_model.cdf([ur, hr], lower_integration_limit=(0, 0))
+        lam = jointAB / alpha
         print(f'{file_labels[i]}, empircal lambda = {lam:.3f}')
